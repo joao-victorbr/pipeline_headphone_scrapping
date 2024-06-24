221ed5a48493f144a989b1b6f7ae663b25e85d9f
joao-victorbr <joaovicb.rodrigues@gmail.com>
2024-06-23 23:23:21 -0300

    fix: finalizacao da etapa de extract

--- src/coleta_de_dados/spiders/magazineluiza.py
+++ src/coleta_de_dados/spiders/magazineluiza.py
@@ -9,7 +9,7 @@ class MagazineluizaSpider(scrapy.Spider):
     max_page = 5
 
     def parse(self, response):
-        products_list = response.css('li.sc-kTbCBX.ciMFyT')
+        products_list = response.css('a.sc-eBMEME.uPWog.sc-gZfzYS.lndZOc.sc-gZfzYS.lndZOc')
         for product in products_list:
             try:
                 reviews_score = product.css("span.sc-epqpcT.jdMYPv::text").get().replace("(","").replace(")","").split(' ')[0]
@@ -26,7 +26,7 @@ class MagazineluizaSpider(scrapy.Spider):
 
             yield {
                 'product_brand':product.css("a::attr(data-brand)").get(),
-                'product_name':product.css("h2.sc-fvwjDU.fbccdO::text").get(),
+                'product_name':product.css("h2.sc-dcjTxL.jXuKxj::text").get(),
                 'reviews_score':reviews_score,
                 'reviews_quantity':reviews_quantity if reviews_score else None,
                 'old_price':old_price,
@@ -34,11 +34,11 @@ class MagazineluizaSpider(scrapy.Spider):
             }
 
         if self.page_count < self.max_page:
+
             self.page_count += 1
             page_number = str(self.page_count)
             next_page_url = 'https://www.magazineluiza.com.br/'+response.css('ul.sc-satoz.kbPvPg a::attr(href)').get()+'&page='+page_number
-            print('')
-            print(next_page_url)
+            print(f'\nScrapping da página {self.page_count-1} concluído.\nIniciando scrapping da página {self.page_count}. Link:',next_page_url)
             print('')
             if next_page_url:
                 yield scrapy.Request(url=next_page_url, callback=self.parse)
